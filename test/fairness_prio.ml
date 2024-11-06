f66563f5b24f6e270e0ad01b0d47f7cf6b79390d
Enrico Tassi <Enrico.Tassi@Inria.fr>
2024-11-06 21:13:59 +0100

    tests

--- test/fairness_prio.ml
+++ test/fairness_prio.ml
@@ -58,17 +58,16 @@ let read_leftover read n =
 (* pop_opt terminates *)
 let%test_unit "sel.loop" =
   let read, write = pipe () in
-  let e = On.line ~priority:1 read (fun x -> x) in
-  write "aa\nbb\ncc\n";
-  let read2, write2 = pipe () in
-  let x = On.bytes ~priority:2 read2 2 (function Error e -> Error e | Ok s -> Error (Failure (Stdlib.Format.asprintf "lower priority event triggered: '%s'" (Bytes.to_string s)))) in
+  let e = On.httpcle ~priority:1 read (fun x -> Result.map ~f:Bytes.to_string x) in
+  write "Content-Length: 3\n\n123\n";
+  let x = Sel.now ~priority:2 (Ok "bad") in
   let todo = Todo.add Todo.empty [e;x] in
   let rec loop todo =
-    let ready, todo = Sel.pop todo in
+    let ready, _todo = Sel.pop todo in
     match ready with
-    | Ok "cc" -> ()
-    | Ok s -> write2 s; loop (Todo.add todo [e])
-    | Error End_of_file -> ()
+    | Ok "bad" -> [%test_eq: string] "" "bad1"
+    | Ok "123" -> ()
+    | Ok _ -> [%test_eq: string] "" "bad2"
     | Error e -> [%test_eq: string] "" (Stdlib.Printexc.to_string e) in
   loop todo
   
